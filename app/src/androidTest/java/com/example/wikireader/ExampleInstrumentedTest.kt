3d44e91050344fd42678edcdc708326a48cc0b27
nsh07 <66912344+nsh07@users.noreply.github.com>
2024-10-01 11:18:01 +0530

    Fix package name change related errors

--- app/src/androidTest/java/com/example/wikireader/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/wikireader/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.nsh07.wikireader", appContext.packageName)
+        assertEquals("org.nsh07.wikireader", appContext.packageName)
     }
 }
