12a06f522ec1739b89f2998273edbd199a1018c8
nsh07 <66912344+nsh07@users.noreply.github.com>
2024-10-16 20:08:30 +0530

    Make image zoomable
    The fullscreen image view is now zoomable. The zoom behaviour is
    a bit inconsistent, will fix it later

--- app/src/main/java/org/nsh07/wikireader/ui/FullScreenImage.kt
+++ app/src/main/java/org/nsh07/wikireader/ui/FullScreenImage.kt
@@ -1,13 +1,11 @@
 package org.nsh07.wikireader.ui
 
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -16,21 +14,21 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import coil.ImageLoader
-import coil.compose.SubcomposeAsyncImage
-import coil.decode.SvgDecoder
-import coil.request.ImageRequest
+import org.nsh07.wikireader.R
 import org.nsh07.wikireader.data.WikiPhoto
 import org.nsh07.wikireader.data.WikiPhotoDesc
-import org.nsh07.wikireader.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -62,41 +60,26 @@ fun FullScreenImage(
         containerColor = Color.Black,
         modifier = modifier
     ) { _ ->
-        val context = LocalContext.current
+        var scale by remember { mutableFloatStateOf(1f) }
+        var offset by remember { mutableStateOf(Offset.Zero) }
+        val state = rememberTransformableState { zoomChange, offsetChange, _ ->
+            scale = (scale * zoomChange).coerceIn(1f..8f)
+            offset += offsetChange * scale
+        }
+
         Box(modifier = Modifier.fillMaxSize()) {
-            SubcomposeAsyncImage(
-                model = ImageRequest.Builder(context)
-                    .data(photo.source)
-                    .crossfade(true)
-                    .build(),
-                loading = {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .aspectRatio(
-                                photo.width.toFloat() / photo.height.toFloat()
-                            )
-                    ) {
-                        CircularProgressIndicator(
-                            modifier = Modifier.align(Alignment.Center)
-                        )
-                    }
-                },
-                error = {
-                    Icon(
-                        painterResource(R.drawable.error),
-                        contentDescription = "Error loading image",
-                        modifier = Modifier.padding(vertical = 16.dp)
-                    )
-                },
-                contentDescription = photoDesc.description[0],
-                imageLoader = ImageLoader.Builder(LocalContext.current)
-                    .components {
-                        add(SvgDecoder.Factory())
-                    }
-                    .build(),
-                contentScale = ContentScale.Fit,
+            PageImage(
+                photo = photo,
+                photoDesc = photoDesc,
+                contentScale = ContentScale.Inside,
                 modifier = Modifier
+                    .graphicsLayer(
+                        scaleX = scale,
+                        scaleY = scale,
+                        translationX = offset.x,
+                        translationY = offset.y
+                    )
+                    .transformable(state = state)
                     .align(Alignment.Center)
             )
         }
