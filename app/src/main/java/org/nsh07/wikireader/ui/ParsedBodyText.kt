87885aba0bc3feb91d733b57dffe12e4c9334458
nsh07 <66912344+nsh07@users.noreply.github.com>
2024-11-19 20:28:15 +0530

    Improve hyperlinks in disambiguation page
    The links and text closely resembles Wikipedia's web interface now (#11)

--- app/src/main/java/org/nsh07/wikireader/ui/ParsedBodyText.kt
+++ app/src/main/java/org/nsh07/wikireader/ui/ParsedBodyText.kt
@@ -1,16 +1,17 @@
 package org.nsh07.wikireader.ui
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withLink
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -34,34 +35,44 @@ fun ParsedBodyText(
             modifier = Modifier.padding(16.dp)
         )
     else
-        FlowRow(
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
-        ) {
-            body.split('\n').forEachIndexed { ind, it ->
-                if (it.contains("===") || it.trim() == "" || it.last() == ':' || (intro && ind == 0))
-                    Text(
-                        text = it,
-                        style = MaterialTheme.typography.bodyLarge,
-                        fontSize = fontSize.sp,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                else if (it.substringBefore(',').trim('"') == pageTitle)
-                    FilledTonalButton(onClick = { onLinkClick(it) }) {
-                        Text(
-                            text = it,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis
-                        )
+        Text(
+            buildAnnotatedString {
+                body.split('\n').forEachIndexed { ind, curr ->
+                    if (curr.contains("===") || curr.trim() == "" || curr.last() == ':' || (intro && ind == 0))
+                        append(curr + "\n")
+                    else {
+                        val link =
+                            LinkAnnotation.Url(
+                                "",
+                                TextLinkStyles(
+                                    SpanStyle(
+                                        color = MaterialTheme.colorScheme.primary,
+                                        textDecoration = TextDecoration.Underline
+                                    )
+                                )
+                            ) {
+                                if (curr.substringBefore(',').trim('"') == pageTitle)
+                                    onLinkClick(curr)
+                                else
+                                    onLinkClick(curr.substringBefore(','))
+                            }
+
+                        if (curr[0] == '"') {
+                            withLink(link) { append(curr.substringBeforeLast('"') + '"') }
+                            append(curr.substringAfterLast('"') + '\n')
+                        } else if (curr.contains(')')) {
+                            withLink(link) { append(curr.substringBefore(')') + ')') }
+                            append(curr.substringAfter(')') + '\n')
+                        } else {
+                            withLink(link) { append(curr.substringBefore(',')) }
+                            append(',' + curr.substringAfter(',') + '\n')
+                        }
                     }
-                else
-                    FilledTonalButton(onClick = { onLinkClick(it.substringBefore(',')) }) {
-                        Text(
-                            text = it.substringBefore(','),
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis
-                        )
-                    }
-            }
-        }
+                }
+            },
+            style = MaterialTheme.typography.bodyLarge,
+            fontSize = fontSize.sp,
+            lineHeight = (24 * (fontSize / 16.0)).toInt().sp,
+            modifier = Modifier.padding(16.dp)
+        )
 }
