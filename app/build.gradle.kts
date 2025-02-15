ce4df059d40bd39f9b3bf4fdf775fc819872dfab
nsh07 <66912344+nsh07@users.noreply.github.com>
2025-02-15 10:31:28 +0530

    Bump version string, update changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "org.nsh07.wikireader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "1.9.0"
+        versionCode = 21
+        versionName = "1.9.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
