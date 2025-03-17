2fa2d9221ee05765457f48a5a68d476029783554
nsh07 <66912344+nsh07@users.noreply.github.com>
2025-03-17 22:24:32 +0530

    Bump version string, add links to Wikipedia in AboutScreen.kt
    The logo in the about screen now also shows whether an alpha version is installed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "org.nsh07.wikireader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 27
-        versionName = "1.10.3"
+        versionCode = 25
+        versionName = "2.0.0-01-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/java/org/nsh07/wikireader/ui/aboutScreen/AboutScreen.kt
+++ app/src/main/java/org/nsh07/wikireader/ui/aboutScreen/AboutScreen.kt
@@ -27,7 +27,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.BaselineShift
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.window.core.layout.WindowSizeClass
@@ -45,6 +51,7 @@ fun AboutScreen(
 ) {
     val uriHandler = LocalUriHandler.current
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    val isAlpha = remember { BuildConfig.VERSION_NAME.contains('a') }
     val weight = remember {
         if (windowSizeClass.windowWidthSizeClass == WindowWidthSizeClass.MEDIUM ||
             windowSizeClass.windowWidthSizeClass == WindowWidthSizeClass.EXPANDED
@@ -77,7 +84,19 @@ fun AboutScreen(
                         Row(verticalAlignment = Alignment.CenterVertically) {
                             Icon(painterResource(R.drawable.ic_launcher_foreground), null)
                             Text(
-                                "WikiReader",
+                                if (!isAlpha) AnnotatedString("WikiReader")
+                                else buildAnnotatedString {
+                                    append("WikiReader")
+                                    withStyle(
+                                        SpanStyle(
+                                            fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                                            fontStyle = FontStyle.Italic,
+                                            baselineShift = BaselineShift.Superscript
+                                        )
+                                    ) {
+                                        append(" αlpha")
+                                    }
+                                },
                                 style = MaterialTheme.typography.headlineSmall,
                                 fontWeight = FontWeight.Bold
                             )
@@ -121,6 +140,45 @@ fun AboutScreen(
                             supportingContent = { Text("Check out older and newer versions") },
                             modifier = Modifier.clickable(onClick = {
                                 uriHandler.openUri("https://github.com/nsh07/WikiReader/releases")
+                            })
+                        )
+                    }
+                }
+                OutlinedCard(modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            "Wikipedia",
+                            style = MaterialTheme.typography.labelLarge,
+                            color = MaterialTheme.colorScheme.primary,
+                            modifier = Modifier
+                                .padding(16.dp)
+                                .align(Alignment.Start)
+                        )
+                        ListItem(
+                            leadingContent = {
+                                Icon(
+                                    painterResource(R.drawable.wikipedia_s_w), null
+                                )
+                            },
+                            headlineContent = { Text("Wikipedia") },
+                            supportingContent = { Text("Visit the Wikipedia website") },
+                            modifier = Modifier.clickable(onClick = {
+                                uriHandler.openUri("https://wikipedia.org")
+                            })
+                        )
+                        ListItem(
+                            leadingContent = {
+                                Icon(
+                                    painterResource(R.drawable.wikimedia_logo_black), null
+                                )
+                            },
+                            headlineContent = { Text("Support Wikipedia") },
+                            supportingContent = { Text("Donate or contribute to Wikipedia") },
+                            modifier = Modifier.clickable(onClick = {
+                                uriHandler.openUri("https://wikimediafoundation.org/support/")
                             })
                         )
                     }
