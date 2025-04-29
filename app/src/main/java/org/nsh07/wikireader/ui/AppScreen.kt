731a5fc586fecf8b669256e8327925257cb6df01
Nishant Mishra <66912344+nsh07@users.noreply.github.com>
2025-04-29 22:45:30 +0530

    Add a status bar scrim to make icons readable
    Closes #145

--- app/src/main/java/org/nsh07/wikireader/ui/AppScreen.kt
+++ app/src/main/java/org/nsh07/wikireader/ui/AppScreen.kt
@@ -8,13 +8,16 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -44,8 +47,11 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.core.text.parseAsHtml
@@ -358,6 +364,8 @@ fun AppScreen(
                         .fillMaxSize()
                         .padding(top = insets.calculateTopPadding())
                 )
+
+                StatusBarProtection()
             }
         }
 
@@ -494,6 +502,28 @@ fun DeleteHistoryItemDialog(
         }
     }
 }
+
+@Composable
+private fun StatusBarProtection(
+    color: Color = MaterialTheme.colorScheme.surface,
+    heightProvider: () -> Float = calculateGradientHeight(),
+) {
+    Canvas(Modifier.fillMaxSize()) {
+        val calculatedHeight = heightProvider()
+        drawRect(
+            color = color.copy(alpha = 0.8f),
+            size = Size(size.width, calculatedHeight),
+        )
+    }
+}
+
+@Composable
+fun calculateGradientHeight(): () -> Float {
+    val statusBars = WindowInsets.statusBars
+    val density = LocalDensity.current
+    return { statusBars.getTop(density).times(1.2f) }
+}
+
 
 @Serializable
 data class Home(
