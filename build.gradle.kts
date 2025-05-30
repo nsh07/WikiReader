4c75fd39e7a6721d84693dd58eb981eb4783c5d4
De General <aubynsamuel05@gmail.com>
2025-05-30 03:52:43 +0000

    feat: Add Baseline Profile for improved startup and runtime performance

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
     alias(libs.plugins.jetbrains.compose.compiler) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.jetbrains.kotlin.serialization) apply false
+    alias(libs.plugins.android.test) apply false
+    alias(libs.plugins.baselineprofile) apply false
 }
