0eb9221fc5dd52832bca4570b844df3454d305c9
Nishant Mishra <nishant.28@outlook.com>
2025-08-03 22:54:26 +0530

    chore: Add Navigation 3 dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,6 +91,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
 
+    implementation(libs.androidx.navigation3.ui)
+    implementation(libs.androidx.navigation3.runtime)
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
